c2efffc24ffffbcbeaf56a8601481689ba5956a8
asdqwertzy <asdf666@mail.com>
2023-09-13 14:05:41 +0200

    Envelop form shit into form tag, add submit handling function

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 var myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  var newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+function ltb() {
+  myLibrary.forEach((book) => console.log(book.title, book.author, book.pages, book.read))
+}
+
+function newBook() {
 
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
 
-function addBookToLibrary(title, author, pages, read) {
-    var newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-  }
+  let title = document.getElementById('title');
+  let author = document.getElementById('author');
+  let pages = document.getElementById('pages');
+  let read = document.getElementById('read');
 
-  function ltb() {
-    myLibrary.forEach((book) => console.log(book.title, book.author, book.pages, book.read))
-  }
-
-  function newBook() {
-    
-  }
+  console.log(`Title: ${title.value}, Author: ${author.value}, Pages: ${pages.value}, Read: ${read.checked}`);
+  title.value = ""
+  author.value = ""
+  pages.value = ""
+  read.checked = false;
+}
