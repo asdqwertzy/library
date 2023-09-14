a3a413b911ac3ce779a630766af1b6f9525a00ea
asdqwertzy <asdf666@mail.com>
2023-09-15 01:18:18 +0200

    Add submit and removal button functionality, grid layout for cards, minor styling changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var myLibrary = [];
 
+// Object constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,18 +8,12 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+
 function addBookToLibrary(title, author, pages, read) {
   var newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
 }
 
-function ltb() {
-  myLibrary.forEach((book) => console.log(book.title, book.author, book.pages, book.read))
-}
-
-function newBook() {
-
-}
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -28,9 +23,59 @@ function handleSubmit(event) {
   let pages = document.getElementById('pages');
   let read = document.getElementById('read');
 
+  // Input sanitization
+  let pagesValue = parseInt(pages.value);
+  if (isNaN(pagesValue) || pagesValue <= 0) {
+    alert('Page count can\'t be negative.');
+    return;
+  }
+
+
+  addBookToLibrary(title.value, author.value, pages.value, read.checked ? 'Yes' : 'No');
+
+  // Create a card element and append it to the content area
+  var card = document.createElement('div');
+  card.classList.add('card')
+  card.innerHTML = `
+  <p class="title"><span class="label">Title:</span> ${title.value}</p>
+  <p class="author"><span class="label">Author:</span> ${author.value}</p>
+  <p class="pages"><span class="label">Pages:</span> ${pages.value}</p>
+  <p class="read"><span class="label">Read?:</span> ${read.checked ? 'Yes' : 'No'}</p>
+  <div class="icons">
+    <button class="icon-button read-button" title="Read toggle"></button>
+    <div class="right"><button class="icon-button edit-button" title="Edit this entry"></button>
+    <button class="icon-button remove-button" title="Remove this entry"></button></div>
+  </div>
+  `;
+
+  var newIndex = myLibrary.length - 1;
+  card.setAttribute('data-id', newIndex);
+
+  const content = document.querySelector('.content');
+  content.appendChild(card);
+
+
+
+  // Reset input fields
   console.log(`Title: ${title.value}, Author: ${author.value}, Pages: ${pages.value}, Read: ${read.checked}`);
   title.value = ""
   author.value = ""
   pages.value = ""
   read.checked = false;
+
 }
+
+function removeBook(element) {
+  var dataIndex = element.getAttribute('data-id');
+  element.remove();
+  myLibrary.splice(dataIndex, 1);
+}
+
+document.addEventListener('click', function (event) {
+  if (event.target.classList.contains('remove-button')) {
+    const card = event.target.closest('.card');
+    if (card) {
+      removeBook(card);
+    }
+  }
+});
